5a14b51883e5b18365b7ecd7afbb0817438cc079
Arno Zeelmaekers <arnozeel@gmail.com>
2019-05-28 15:18:09 +0200

    Refactoring multi uav

---
+++ src/multi_uav_px4_ros.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import rospy
+import os
+import json
+
+from sensor_msgs.msg import Imu
+from mavros import mavlink
+from mavros_msgs.msg import Mavlink, Waypoint, WaypointReached, State
+from mavros_msgs.srv import WaypointPush, SetMode, CommandBool
+from pymavlink import mavutil
+from threading import Thread
